9d79066981d647ef560318dfd122fcfdbab1aaac
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-07-17 12:18:34 -0300

    componente de notification-button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
+import NotifficationButton from "./components/notificationButton"
 
 
 function App() {
 
 
   return (
+    <>
     <h1>Olá mundo!</h1>
+    <NotifficationButton />
+    </>
   )
 }
 
